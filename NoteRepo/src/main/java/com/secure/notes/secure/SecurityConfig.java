63ac07c6c67e823820f47566e543892bdb8d696a
Sunilashokbhoi <sunilbhoi606@gmail.com>
2025-10-11 18:22:30 +0530

    MODIFIED: Configure CSRF Protection token

--- NoteRepo/src/main/java/com/secure/notes/secure/SecurityConfig.java
+++ NoteRepo/src/main/java/com/secure/notes/secure/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import java.time.LocalDate;
 
@@ -28,13 +29,17 @@ import java.time.LocalDate;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+     http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+             .ignoringRequestMatchers("/api/auth/public/**")
+     );
+        // http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests((request)
                 -> request
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                .requestMatchers("/api/csrf-token").permitAll()
                 .anyRequest().authenticated());
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.addFilterBefore(new CustomeLoggingFilter(), UsernamePasswordAuthenticationFilter.class);
-        http.addFilterAfter(new RequestValidationFilter(), CustomeLoggingFilter.class);
+
         http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
